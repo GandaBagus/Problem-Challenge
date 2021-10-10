ca3abc5abe9cc8ffce8169ad5a5c50a067bffc0a
GandaBagus <190155201039@student.umrah.ac.id>
2021-10-10 20:15:34 +0700

    ballcontrol

--- Assets/Script/BallControl.cs
+++ Assets/Script/BallControl.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class BallControl : MonoBehaviour
 {
+    // Titik asal lintasan bola saat ini
+    private Vector2 trajectoryOrigin;
+    // Untuk mengakses informasi titik asal lintasan
 
     // Rigidbody 2D bola
     private Rigidbody2D rigidBody2D;
@@ -12,7 +14,22 @@ public class BallControl : MonoBehaviour
     // Besarnya gaya awal yang diberikan untuk mendorong bola
     public float xInitialForce;
     public float yInitialForce;
+    // Start is called before the first frame update
 
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+
+
+    // Untuk mengakses informasi titik asal lintasan
 
     void ResetBall()
     {
@@ -52,18 +69,16 @@ public class BallControl : MonoBehaviour
         // Setelah 2 detik, berikan gaya ke bola
         Invoke("PushBall", 2);
     }
-    // Start is called before the first frame update
+
     void Start()
     {
+        trajectoryOrigin = transform.position;
         rigidBody2D = GetComponent<Rigidbody2D>();
 
         // Mulai game
         RestartGame();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }

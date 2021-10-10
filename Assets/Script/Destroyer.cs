9b43083bd467acbee21afd8777596da3f1c92935
GandaBagus <190155201039@student.umrah.ac.id>
2021-10-10 22:25:09 +0700

    push

---
+++ Assets/Script/Destroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        ScoreScript.scoreValue += 10;
+        if (other.gameObject.tag == "Spawnable")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

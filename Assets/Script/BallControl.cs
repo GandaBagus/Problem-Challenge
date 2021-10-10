97da6a473d51aa762ff99da5395bf7738f8db134
GandaBagus <190155201039@student.umrah.ac.id>
2021-10-10 20:29:26 +0700

    ballcontrol.cs

--- Assets/Script/BallControl.cs
+++ Assets/Script/BallControl.cs
@@ -1,105 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class BallControl : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    //public float xInitialForce;
-    //public float yInitialForce;
 
-    public float speed;
-    public const string Right = "right";
-    public const string Left = "left";
-    public const string Up = "up";
-    public const string Down = "down";
+    
+    public float moveSpeed = 0.1f;
 
-    string buttonPressed;
-
-    private void Start()
+    // Use this for initialization
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
 
-        RestartGame();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            buttonPressed = Up;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            buttonPressed = Down;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            buttonPressed = Left;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            buttonPressed = Right;
-        }
-        else
-        {
-            buttonPressed = null; ;
-        }
-    }
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
 
-    void FixedUpdate()
-    {
-        if (buttonPressed == Right)
-        {
-            rb.AddForce(new Vector2(speed, 0));
-        }
-        else if (buttonPressed == Left)
-        {
-            rb.AddForce(new Vector2(-speed, 0));
-        }
-        else if (buttonPressed == Up)
-        {
-            rb.AddForce(new Vector2(0, speed));
-        }
-        else if (buttonPressed == Down)
-        {
-            rb.AddForce(new Vector2(0, -speed));
-        }
-        else
-        { }
-    }
-
-    void ResetBall()
-    {
-        //reset posisi circle
-        transform.position = Vector2.zero;
-
-        //reset kecepatan circle
-        rb.velocity = Vector2.zero;
-    }
-
-    //void PushBall()
-    // {
-         //untuk random arah bola
-         //float randomDirection = Random.Range(0,2);
-
-         //Jika nilai random dibawah 1 bola ke kekiri
-         //selain itu bola bergerak kekanan
-        // if(randomDirection < 1.0f)
-         //{
-           //  rb.AddForce(new Vector2(-xInitialForce, -yInitialForce));
-        // }else
-         //{
-           //  rb.AddForce(new Vector2(xInitialForce, yInitialForce));
-        // }
-     //}
-
-    void RestartGame()
-    {
-        //kembali ke posisi semula
-        ResetBall();
-
-        //bola bergerak
-        //Invoke("PushBall", 0.01f);
     }
 }

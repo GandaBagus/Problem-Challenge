d70fb67ed46da822f16b033f33fdd37fcd2641c8
GandaBagus <190155201039@student.umrah.ac.id>
2021-10-10 23:51:41 +0700

    push

--- Assets/Script/Destroyer.cs
+++ Assets/Script/Destroyer.cs
@@ -3,17 +3,21 @@
 public class Destroyer : MonoBehaviour
 {
     public Spawner sp;
+    public AudioSource tickSource;
 
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         ScoreScript.scoreValue += 10;
         if (other.gameObject.tag == "Spawnable")
         {
+            
             Destroy(other.gameObject);
             sp.spawnObject();
-        }
-        
-           
-       
+            tickSource.Play();
+        }    
     }
 }
